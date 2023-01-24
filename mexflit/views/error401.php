d3f885741d0f64b4fa3a1ae9a0ca2ae0df63f155
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-01-24 09:21:12 -0500

    Proyecto Terminado

--- mexflit/views/error401.php
+++
@@ -1,7 +0,0 @@
-<?php
-print('
-    <h2 class="p1 error">Error 401: No tienes autorizacion para este recurso</h2>
-    <img class="img-404" src="./public/img/not-found.png" alt="Recurso no encontrado" width="500px">
-
-');
-?>
